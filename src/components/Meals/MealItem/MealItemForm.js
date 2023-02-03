5dd0d86a6d9fe588c6bc055434745558c8c05db7
frankkrijnen <fjkrijnen@gmail.com>
2023-02-03 22:47:55 +0100

    Added refs and logic for cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
     <Fragment>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <Input
           label="Amount"
+          ref={amountInputRef}
           input={{
             id: "amount_" + props.id,
             type: "number",
@@ -17,6 +36,7 @@ const MealItemForm = (props) => {
           }}
         />
         <button>+ Add</button>
+        {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
       </form>
     </Fragment>
   );
